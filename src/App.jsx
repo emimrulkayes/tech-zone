0021c681fa28af29b3339a966fc8334f59bcb79c
emimrulkayes <emimrulkayes@gmail.com>
2024-10-11 02:53:10 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-fuchsia-600"> Tech zone - React & Vite Project</h1>
+      
+    </>
+  )
+}
+
+export default App
